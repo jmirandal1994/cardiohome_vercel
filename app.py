2888ecb1d25484e563dd7ba742fba15ac2b9b89e
jmirandal1994 <jmiraandal@gmail.com>
2026-02-07 12:16:17 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1732,8 +1732,8 @@ def admin_cargar_nomina():
         "coord_general_id": coord_general_id_db,
         "coord_escuela_id": coord_escuela_id_db,
         "token_acceso": token_generado,
-        "establecimiento_id": None 
-        "proyecto_id": proyecto_id_db,     # <--- AÑADIR ESTA LÍNEA        # -----------------------------------------------------------
+        "establecimiento_id": None
+        "proyecto_id": proyecto_id_db,
     }
     
     try:
